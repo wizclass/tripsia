5f75d275e1d0ab1bafbf72c63374903e5b08c8be
soo-willsoft <soo@willsoft.kr>
2023-08-31 10:32:21 +0900

    fixed untracked files

--- m3cron/dbconfig.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $host_name = '127.0.0.1';
-    $user_name = 'root';
-    $user_pwd = 'wizclass.inc@gmail.com';
-    // $user_pwd = 'willsoft0780!@';
-    $database = 'tripsia';
-    return mysqli_connect($host_name,$user_name,$user_pwd,$database);
-?>
